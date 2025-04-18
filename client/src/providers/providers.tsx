8df7657ae17c46c2f47315dcc2c3515fce682e45
mohit838 <mohitul449@gmail.com>
2025-04-18 21:19:44 +0600

    update prividers for routes

--- client/src/providers/providers.tsx
+++ client/src/providers/providers.tsx
@@ -2,15 +2,17 @@ import theme from '@/styles/theme'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ConfigProvider } from 'antd'
 import { ReactNode } from 'react'
-
+import { BrowserRouter } from 'react-router-dom'
 
 const AppProviders = ({ children }: { children: ReactNode }) => {
     const queryClient = new QueryClient()
 
     return (
-        <QueryClientProvider client={queryClient}>
-            <ConfigProvider theme={theme}>{children}</ConfigProvider>
-        </QueryClientProvider>
+        <BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+                <ConfigProvider theme={theme}>{children}</ConfigProvider>
+            </QueryClientProvider>
+        </BrowserRouter>
     )
 }
 
